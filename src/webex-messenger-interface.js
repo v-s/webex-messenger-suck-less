6283c089960025f8413a295f6446ef7cdd5e88fc
v-s <vikasnair@gmail.com>
2015-03-12 16:29:10 -0700

    Fix:   * An incomplete cleanup was causing the UI-scrapping code to never get invoked.

--- src/webex-messenger-interface.js
+++ src/webex-messenger-interface.js
@@ -5,35 +5,33 @@ globalVarsAccessHackScript.onload = function() {
     globalVarsAccessHackScript.parentNode.removeChild(globalVarsAccessHackScript);
 };
 
-document.addEventListener('webexMessengerSuckLess_globalVarAccessHack', function(e) {
-  chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      var loggedInUser = $('#webim_main_showuser').attr('title');
-    	var newMessages = {};
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    var loggedInUser = $('#webim_main_showuser').attr('title');
+  	var newMessages = {};
 
-      $('div.IMDialog.webim_onDialogNewMsgReceived').each(function() {
-        var thisElt = $(this);
-        var sender = thisElt.find('div.userName').text();
+    $('div.IMDialog.webim_onDialogNewMsgReceived').each(function() {
+      var thisElt = $(this);
+      var sender = thisElt.find('div.userName').text();
 
-        thisElt.find('div.msg').each(function() {
-          var msgElt = $(this);
+      thisElt.find('div.msg').each(function() {
+        var msgElt = $(this);
 
-          if(msgElt.siblings('div.buddyName').text().match(new RegExp('^' + loggedInUser + '.*', 'i'))) {
+        if(msgElt.siblings('div.buddyName').text().match(new RegExp('^' + loggedInUser + '.*', 'i'))) {
+          newMessages[sender] = [];
+        } else {
+          if (!newMessages[sender]) {
             newMessages[sender] = [];
-          } else {
-            if (!newMessages[sender]) {
-              newMessages[sender] = [];
-            }
-
-            var message = {};
-            message.text = msgElt.find('span.msgText').text().trim();
-            message.timestamp = msgElt.find('span.timeStamp').text().trim();
-            newMessages[sender].push(message);
           }
-        });
-    	});
 
-    	sendResponse({messages : newMessages});
-    }
-  );
-});
+          var message = {};
+          message.text = msgElt.find('span.msgText').text().trim();
+          message.timestamp = msgElt.find('span.timeStamp').text().trim();
+          newMessages[sender].push(message);
+        }
+      });
+  	});
+
+  	sendResponse({messages : newMessages});
+  }
+);
